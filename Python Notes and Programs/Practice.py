8b46be33438d00e8c375fe028cb69e94fa44da6e
Sanjeev Kumar Prajapati <69379895+SanjeevKumarPrajapati@users.noreply.github.com>
2021-10-23 23:24:55 +0530

    Add files via upload

---
+++ Python Notes and Programs/Practice.py
@@ -0,0 +1,78 @@
+"""
+#<----------------operators------------------->
+#1.) Arithmetic Operator (+,-,/,//,**,%)
+
+a=10
+b=20
+print("1.) Arithmetic Operator (+,-,/,//,**,%)\n")
+print("Addition Operation :- ",a+b)
+print("Subtraction :- ",a-b)
+print("Division :- ",a/b)
+print("Multiplication :- ",a*b)
+print("Exponent :- ",a**b)
+print("Floor Division :-",a//b)
+
+#2.) Relational operators(==,>,<,>=,<=,!=)
+
+print("1.) Relational operators(==,>,<,>=,<=,!=)\n")
+c=10
+b=12
+print("Equal to operation :- ",c==b)
+print("Greater than :-",c>b)
+print("Less than :- ",c<b)
+print("Greater than and equal to :-",c>=b)
+print("Less than and equal to :- ",c<=b)
+print("Not Equal to :- ",c!=b)
+
+print("1.) Assignment operators(+=,-=,*=,%=,**=)\n")
+d=10
+d+=2#d=d+2
+print("Addition :- ",d)
+d-=2#d=12
+print("Subtraction :- ",d)#10
+d/=2 #d=d/2,10/2=5
+print("Division :- ",d)
+d*=10
+print("Multiplication :-",d)
+d**=2#d=d^2,50^2
+print("Exponent :-",d)
+
+print("1.) Logical operators(and , or , not)\n")
+e=True
+f=True
+print("And Operator :- ",e and f)
+
+print("Or Operator :- ",e or f)
+
+print("Not operator :- ",e)
+print("Not Operators :- ",f)
+
+print("1.) Bitwise operators(&,|,^,~)\n")
+a=10
+b=15
+print("And Bitwise operator :- ",a&b)
+print("Or Bitwise Operator :- ",a|b)
+print("Xor Bitwise operator :- ",a^b)
+print("Compliment :- ",~a)
+
+
+"""
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
