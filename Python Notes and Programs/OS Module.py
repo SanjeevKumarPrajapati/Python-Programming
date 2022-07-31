92238f9ee7c9858b2bbb8b43aba3cd980efb2250
Sanjeev Kumar Prajapati <69379895+SanjeevKumarPrajapati@users.noreply.github.com>
2022-07-30 21:36:11 -0700

    Add files via upload

---
+++ Python Notes and Programs/OS Module.py
@@ -0,0 +1,17 @@
+#Create a directory
+import os
+#os.mkdir('newdir')
+
+# Get current directory
+#print(os.getcwd())
+
+#Determine the name of the operating system
+#print(os.name)
+#posix(Portable operating system interface)(unix)
+#nt(new technology)
+#ce(microsoft embedding system)
+#java(Java Vartual Machine)
+
+#Remove a directory
+#os.rmdir('newdir')
+
